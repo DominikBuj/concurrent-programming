2ed473115566181a95b14ab5089733e0703ccb1e
DominikBuj <dominik.bujnowicz2@gmail.com>
2022-12-14 19:19:14 +0100

    adding tailwind and testing layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
-function App() {
+const fileFoldersCount = 5;
+const fileFolders = () => [...Array(fileFoldersCount)].map((value, index) => <div>{index}</div>);
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Help me.</h1>
+    <div className='grid grid-rows-3'>
+      <div className='row-span-1 grid grid-cols-5 bg-teal-200 FileFoldersContainer'>{fileFolders()}</div>
+      <div className='row-span-2 bg-teal-800 ClientsContainer'></div>
     </div>
   );
 }
-
-export default App;
