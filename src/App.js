1ab8d34d6bb0e307550b7b902f504b0ed6d02da3
DominikBuj <dominik.bujnowicz2@gmail.com>
2023-01-13 23:59:40 +0100

    small style changes and removing junk

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
-import './App.css';
 
-let started = false;
+let clientCounter = 20;
 
 const random = (min, max) => Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min));
 
-const getFilePixelSize = (fileSize) => (fileSize * 100) + 44;
+const getFilePixelSize = (fileSize) => (fileSize * 120) + 24;
 
 const getFolderObject = (folderIndex) => document.getElementById('folders-container').children[folderIndex].firstElementChild;
 const getFileObject = (clientIndex, fileIndex) => document.getElementById('clients-container').children[clientIndex]?.firstElementChild.children[fileIndex + 1];
 
+
 export default function App() {
 
-  const [windowSize, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+  const [, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight });
 
   const [data, setData] = useState({
     folders: [...Array(5)].map(() => {return {
@@ -21,18 +21,21 @@ export default function App() {
       clientIndex: null,
       fileIndex: null
     }}),
-    clients: [...Array(20)].map(() => {return {
+    clients: [...Array(clientCounter)].map((value, index) => {return {
+        counter: index + 1,
         creationTime: Date.now(),
-        files: [...Array(6)].map(() => {return {
+        files: [...Array(random(1, 5))].map(() => {return {
             size: random(1, 100) * 0.01,
             folderIndex: null
         }}).sort((a, b) => (a.size > b.size) ? 1 : (-1)).reverse()
     }})
   });
 
-  useEffect(() => { handleResize(); handleUploads(); }, []);
+  useEffect(() => { handleResize(); handleUploads(); clientCounter = data.clients.length; }, []);
 
   useEffect(() => drawUploads());
+
+  let started = false;
 
   const handleResize = () => window.addEventListener('resize', () => setWindowSize({ width: window.innerWidth, height: window.innerHeight }));
 
@@ -79,7 +82,7 @@ export default function App() {
 
     });
 
-  };
+  }
 
   function drawUploads() {
 
@@ -88,7 +91,7 @@ export default function App() {
     canvas.height = window.innerHeight;
     const canvasContext = canvas.getContext('2d');
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-    canvasContext.lineWidth = 5;
+    canvasContext.lineWidth = 3;
 
     data.folders.forEach((folder, folderIndex) => {
       if (folder.clientIndex === null || folder.fileIndex === null) return;
@@ -96,11 +99,11 @@ export default function App() {
       const fileRectangle = getFileObject(folder.clientIndex, folder.fileIndex).getBoundingClientRect();
       canvasContext.beginPath();
       canvasContext.moveTo(folderRectangle.left + folderRectangle.width / 2, folderRectangle.bottom - 2);
-      canvasContext.lineTo(fileRectangle.left + fileRectangle.width / 2, fileRectangle.top + 2);
+      canvasContext.lineTo(fileRectangle.left + fileRectangle.width / 2, fileRectangle.top + 1);
       canvasContext.stroke();
     });
 
-  };
+  }
 
   function getFreeFolderIndex() {
     for (let i = 0; i < data.folders.length; ++i) {
@@ -108,7 +111,7 @@ export default function App() {
         if (folder.clientIndex === null || folder.fileIndex === null) return i;
     }
     return null;
-  };
+  }
 
   function existsFreeFile() {
     for (let i = 0; i < data.clients.length; ++i) {
@@ -155,7 +158,7 @@ export default function App() {
 
       return possibleFiles.sort((a, b) => (a.priority > b.priority) ? 1 : (-1)).pop().combinedIndex;
 
-  };
+  }
 
   function runArbiter() {
 
@@ -184,14 +187,17 @@ export default function App() {
 
       setTimeout(runArbiter, 100);
 
-  };
+  }
 
   function generateClient() {
 
     if (data.clients.length >= 20) return;
 
+    ++clientCounter;
+
     const dataCopy = data;
     dataCopy.clients.push({
+      counter: clientCounter,
       creationTime: Date.now(),
       files: [...Array(random(1, 5))].map(() => {return {
           size: random(1, 100) * 0.01,
@@ -200,14 +206,14 @@ export default function App() {
     });
     setData({...dataCopy});
 
-  };
+  }
 
   return (
     <>
 
       <canvas id="canvas" className="fixed"></canvas>
 
-      <div className="grid grid-cols-5 bg-cyan-100">
+      <div className="grid grid-cols-5 bg-slate-200">
 
         <div className="col-span-4 grid grid-rows-3">
 
@@ -215,7 +221,8 @@ export default function App() {
             {
               [...data.folders].map((value, index) => (
                 <div className="flex justify-center items-center" key={index}>
-                  <div className="bg-yellow-400 w-32 h-32 border-4 text-2xl font-bold border-black rounded text-center align-middle leading-[8rem]"></div>
+                  <div className="bg-yellow-400 w-32 h-32 border-4 text-2xl font-bold border-black
+                  rounded text-center align-middle leading-[8rem]"></div>
                 </div>
               ))
             }
@@ -226,10 +233,11 @@ export default function App() {
               [...data.clients].map((value, index) => (
                 <div className="flex justify-center mb-2" key={index}>
                   <div className="flex flex-col-reverse w-1/2 gap-2">
-                    <div className="h-12 text-2xl font-bold text-center leading-[3rem]">{index + 1}</div>
+                    <div className="h-12 text-2xl font-bold text-center leading-[3rem]">{value.counter}</div>
                     {
                       [...value.files].map((value, index) => (
-                        <div className="bg-blue-300 border-4 rounded border-black" style={{height: `${getFilePixelSize(value.size)}px`}} key={index}></div>
+                        <div className="bg-blue-400 border-2 rounded border-black"
+                          style={{height: `${getFilePixelSize(value.size)}px`}} key={index}></div>
                       ))
                     }
                   </div>
@@ -240,14 +248,16 @@ export default function App() {
 
         </div>
 
-        <div className="border-l-4 border-slate-900 flex flex-col p-2 gap-2">
+        <div className="bg-slate-300 border-l-2 border-black flex flex-col p-2 gap-2">
 
-          <button className="bg-black hover:bg-slate-800 py-2 px-4 rounded z-10" onClick={!started ? runArbiter : null}>
-            <p className="text-2xl text-white font-bold">Start</p>
+          <button className="text-white focus:ring-4 rounded-lg px-5 py-2.5 bg-green-700 hover:bg-green-800
+          focus:outline-none focus:ring-green-800 z-10 text-2xl font-bold" onClick={!started ? runArbiter : null}>
+            Start
           </button>
 
-          <button className="bg-black hover:bg-slate-800 py-2 px-4 rounded z-10" onClick={generateClient}>
-            <p className="text-2xl text-white font-bold">Generuj Klienta</p>
+          <button className="text-white focus:ring-4 rounded-lg px-5 py-2.5 bg-green-700 hover:bg-green-800
+          focus:outline-none focus:ring-green-800 z-10 text-2xl font-bold" onClick={generateClient}>
+            Generuj Klienta
           </button>
 
         </div>
